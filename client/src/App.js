ac124dafee49717874cecdd615ac66dc5c0f11c4
JordanoBaluz <jordanolb95@hotmail.com>
2022-07-18 21:58:25 -0300

    created the function to handle the changes values in the inputs and the function to the button

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [values, setValues] = useState();
+
+  const handleChangeValues = (values) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [values.target.name]: values.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
+  };
+
   return (
     <div className="app-container">
       <div className="register-container">
@@ -10,20 +24,25 @@ function App() {
           name="name"
           placeholder="Nome"
           className="register-input"
+          onChange={handleChangeValues}
         />
         <input
           type="text"
           name="cost"
           placeholder="Preço"
           className="register-input"
+          onChange={handleChangeValues}
         />
         <input
           type="text"
           name="category"
           className="register-input"
           placeholder="Categoria"
+          onChange={handleChangeValues}
         />
-        <button>Cadastrar</button>
+        <button className="register-button" onClick={() => handleClickButton()}>
+          Cadastrar
+        </button>
       </div>
     </div>
   );
