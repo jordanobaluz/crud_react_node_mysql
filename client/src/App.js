e5bc01886136011400aaf91b03be8b8a244727ae
JordanoBaluz <jordanolb95@hotmail.com>
2022-07-18 21:49:28 -0300

    created the structure of the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app-container">
+      <div className="register-container">
+        <h1 className="register-title">Scrim Shop</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register-input"
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register-input"
+        />
+        <input
+          type="text"
+          name="category"
+          className="register-input"
+          placeholder="Categoria"
+        />
+        <button>Cadastrar</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
