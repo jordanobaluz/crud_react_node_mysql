4097d6c855f74449d038ffa7a17bdb8e6c0aa1f2
JordanoBaluz <jordanolb95@hotmail.com>
2022-07-19 21:12:28 -0300

    created the connection with the mysql db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const cors = require("cors");
+
+const mysql = require("mysql2");
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "crudgames",
 });
+
+// app.get("/", (req, res) => {
+//   let SQL =
+//     "INSERT INTO games (name, cost, category) VALUES ('Far Cry 5', '120', 'ação')";
+
+//   db.query(SQL, (err, result) => {
+//     console.log(err);
+//   });
+// });
+
+app.use(cors());
+app.use(express());
 
 app.listen(3001, () => {
   console.log("serv online");
