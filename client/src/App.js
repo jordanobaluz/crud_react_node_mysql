7e573fc06b231b924f6822d0e64bc365b92254af
JordanoBaluz <jordanolb95@hotmail.com>
2022-07-21 21:14:07 -0300

    created card to show the info from database, created the props that pass the data

--- client/src/App.js
+++ client/src/App.js
@@ -60,7 +60,16 @@ function App() {
 
       {typeof listGames !== "undefined" &&
         listGames.map((value) => {
-          return <Card></Card>;
+          return (
+            <Card
+              key={value.id}
+              listCard={listGames}
+              setListCard={setListGames}
+              id={value.id}
+              name={value.name}
+              cost={value.cost}
+              category={value.category}></Card>
+          );
         })}
     </div>
   );
--- client/src/components/cards/card.js
+++ client/src/components/cards/card.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./card.css";
 
-export default function Card() {
-  return <h1>Card</h1>;
+export default function Card(props) {
+  return (
+    <div className="card-container">
+      <h1 className="card-title">{props.name}</h1>
+      <p className="card-category">{props.category}</p>
+      <p className="card-cost">R$ {props.cost}</p>
+    </div>
+  );
 }
